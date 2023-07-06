9586835f6f11e2d2d606c3d68eaba66c23c5cc49
Josh Klope <klope3@msn.com>
2023-07-06 08:34:10 -0500

    Add map to test component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,26 @@
-import { useState, useEffect } from "react";
+import mapboxgl, { Map } from "mapbox-gl";
+import { useState, useEffect, useRef } from "react";
+
+const accesstoken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+if (!accesstoken) console.error("No access token for Mapbox found!");
+else mapboxgl.accessToken = accesstoken;
+const initialLong = -92.4742;
+const initialLat = 42.7293;
 
 export function Test() {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<Map | null>(null);
   const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current!,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [initialLong, initialLat],
+      zoom: 17,
+    });
+  }, []);
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition(
@@ -23,6 +42,7 @@ export function Test() {
 
   return (
     <div>
+      <div ref={mapContainer} style={{ width: "320px", height: "400px" }}></div>
       <h1>Player Location</h1>
       <p>Latitude: {location.latitude}</p>
       <p>Longitude: {location.longitude}</p>
